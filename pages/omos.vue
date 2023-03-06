262f5560ec0617b00877151bd05cdf845460f98b
Kastholm <Kastholm1995@gmail.com>
2023-03-06 12:48:48 +0100

    SplitText Aboutpage

--- pages/omos.vue
+++ pages/omos.vue
@@ -1,22 +1,308 @@
 <template>
-  <section>
-
-
-
-
-
-
-
-     
+  <section
+    class="container about relative m-auto text-center pt-[2em] text-6xl"
+  >
+    <div class="aboutSection">
+      <h1>
+        Filosofien & Værdien bag <br />
+        Musikmekanikeren
+      </h1>
+      <span class="textContainer">
+        <p>
+          Musik Mekanikeren filosofi er, at alle kan få sig et fedt instrument.
+          Hvad enten det er tale om en prisvenlig begynder guitar eller bas, det
+          første trommesæt eller et dyrere & eksklusivt custom shop instrument.
+          Uanset modellen og kvaliteten, er langt de fleste instrumenter
+          sjældent sat ordentligt op fra fabrikken, eller også passer fabrik
+          setup'et ikke til dig.
+        </p>
+        <p>Det er her at Musik Mekanikeren kan hjælpe dig.</p>
+        <p>
+          Udover servicering af dit instrument, tilbyder Musik Mekanikeren også
+          kyndig råd & vejledning omkring alt fra gode tips & tricks til
+          strengetykkelse og trommeskind , med henblik på det optimale setup for
+          dig.
+        </p>
+        <p>
+          På værkstedet bliver der løbende købt nyt og lækkert special-værktøj
+          samt masser af materialer og reservedele ind, udvalget konstant bliver
+          større. Har du en forespørgsel er du meget velkommen til at kontakte
+          værkstedet for et uforpligtende og særskilt tilbud.
+        </p>
+      </span>
+      <img
+        class="imageOne"
+        src="https://musikmekanikeren.dk/____impro/1/onewebmedia/Privat/Musik%20Mekanikeren%20Lodning%20Elektronik.JPG?etag=%221b9763-5be21834%22&sourceContentType=image%2Fjpeg&ignoreAspectRatio&resize=484%2B507&extract=1%2B19%2B482%2B314&quality=85"
+        alt=""
+      />
+    </div>
+    <div class="aboutSection">
+      <h2 class="headerTwo">
+        Filosofien & Værdien bag <br />
+        Musikmekanikeren
+      </h2>
+      <span class="textContainer">
+        <p>
+          Musik Mekanikeren filosofi er, at alle kan få sig et fedt instrument.
+          Hvad enten det er tale om en prisvenlig begynder guitar eller bas, det
+          første trommesæt eller et dyrere & eksklusivt custom shop instrument.
+          Uanset modellen og kvaliteten, er langt de fleste instrumenter
+          sjældent sat ordentligt op fra fabrikken, eller også passer fabrik
+          setup'et ikke til dig.
+        </p>
+        <p>Det er her at Musik Mekanikeren kan hjælpe dig.</p>
+        <p>
+          Udover servicering af dit instrument, tilbyder Musik Mekanikeren også
+          kyndig råd & vejledning omkring alt fra gode tips & tricks til
+          strengetykkelse og trommeskind , med henblik på det optimale setup for
+          dig.
+        </p>
+        <p>
+          På værkstedet bliver der løbende købt nyt og lækkert special-værktøj
+          samt masser af materialer og reservedele ind, udvalget konstant bliver
+          større. Har du en forespørgsel er du meget velkommen til at kontakte
+          værkstedet for et uforpligtende og særskilt tilbud.
+        </p>
+      </span>
+      <img
+        class="imageTwo"
+        src="https://musikmekanikeren.dk/____impro/1/onewebmedia/Privat/Kaares%20f%C3%B8rste%20trommes%C3%A6t.png?etag=%22392709-56a77a82%22&sourceContentType=image%2Fpng&ignoreAspectRatio&resize=458%2B309&extract=0%2B10%2B456%2B296"
+        alt=""
+      />
+    </div>
+    <div class="aboutSection">
+      <h2 class="headerTwo">
+        Filosofien & Værdien bag <br />
+        Musikmekanikeren
+      </h2>
+      <span class="textContainer">
+        <p>
+          Musik Mekanikeren filosofi er, at alle kan få sig et fedt instrument.
+          Hvad enten det er tale om en prisvenlig begynder guitar eller bas, det
+          første trommesæt eller et dyrere & eksklusivt custom shop instrument.
+          Uanset modellen og kvaliteten, er langt de fleste instrumenter
+          sjældent sat ordentligt op fra fabrikken, eller også passer fabrik
+          setup'et ikke til dig.
+        </p>
+        <p>Det er her at Musik Mekanikeren kan hjælpe dig.</p>
+        <p>
+          Udover servicering af dit instrument, tilbyder Musik Mekanikeren også
+          kyndig råd & vejledning omkring alt fra gode tips & tricks til
+          strengetykkelse og trommeskind , med henblik på det optimale setup for
+          dig.
+        </p>
+        <p>
+          På værkstedet bliver der løbende købt nyt og lækkert special-værktøj
+          samt masser af materialer og reservedele ind, udvalget konstant bliver
+          større. Har du en forespørgsel er du meget velkommen til at kontakte
+          værkstedet for et uforpligtende og særskilt tilbud.
+        </p>
+      </span>
+      <img
+        class="imageTwo"
+        src="https://musikmekanikeren.dk/____impro/1/onewebmedia/Privat/Kaares%20f%C3%B8rste%20trommes%C3%A6t.png?etag=%22392709-56a77a82%22&sourceContentType=image%2Fpng&ignoreAspectRatio&resize=458%2B309&extract=0%2B10%2B456%2B296"
+        alt=""
+      />
+    </div>
+    <div class="aboutSection">
+      <h2 class="headerTwo">
+        Filosofien & Værdien bag <br />
+        Musikmekanikeren
+      </h2>
+      <span class="textContainer">
+        <p>
+          Musik Mekanikeren filosofi er, at alle kan få sig et fedt instrument.
+          Hvad enten det er tale om en prisvenlig begynder guitar eller bas, det
+          første trommesæt eller et dyrere & eksklusivt custom shop instrument.
+          Uanset modellen og kvaliteten, er langt de fleste instrumenter
+          sjældent sat ordentligt op fra fabrikken, eller også passer fabrik
+          setup'et ikke til dig.
+        </p>
+        <p>Det er her at Musik Mekanikeren kan hjælpe dig.</p>
+        <p>
+          Udover servicering af dit instrument, tilbyder Musik Mekanikeren også
+          kyndig råd & vejledning omkring alt fra gode tips & tricks til
+          strengetykkelse og trommeskind , med henblik på det optimale setup for
+          dig.
+        </p>
+        <p>
+          På værkstedet bliver der løbende købt nyt og lækkert special-værktøj
+          samt masser af materialer og reservedele ind, udvalget konstant bliver
+          større. Har du en forespørgsel er du meget velkommen til at kontakte
+          værkstedet for et uforpligtende og særskilt tilbud.
+        </p>
+      </span>
+      <img
+        class="imageTwo"
+        src="https://musikmekanikeren.dk/____impro/1/onewebmedia/Privat/Kaares%20f%C3%B8rste%20trommes%C3%A6t.png?etag=%22392709-56a77a82%22&sourceContentType=image%2Fpng&ignoreAspectRatio&resize=458%2B309&extract=0%2B10%2B456%2B296"
+        alt=""
+      />
+    </div>
+    <div class="blur"></div>
   </section>
 </template>
 
-<script>
-export default {
+<script setup>
+import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { onMounted } from "vue";
 
-}
+onMounted(async () => {
+  // Register gsap plugin
+  gsap.registerPlugin(SplitText, ScrollTrigger);
+
+  var tl = gsap.timeline(),
+    mySplitText = new SplitText("h1", { type: "words,chars" }),
+    chars = mySplitText.chars; //an array of all the divs that wrap each character
+
+  gsap.set("h1", { perspective: 400 });
+
+  tl.from(chars, {
+    duration: 0.8,
+    opacity: 0,
+    scale: 0,
+    y: 80,
+    rotationX: 180,
+    transformOrigin: "0% 50% -50",
+    ease: "back",
+    stagger: 0.01,
+  });
+
+  // Add the <p> animation
+  gsap.utils.toArray(".aboutSection p").forEach((p) => {
+    const text = p.textContent;
+    const mySplitText = new SplitText(p, { type: "words,chars" });
+    const chars = mySplitText.chars;
+
+    gsap.set(p, { opacity: 0, y: 50, skewY: 5 });
+    gsap.to(p, {
+      duration: 1,
+      skewY: 0,
+      opacity: 1,
+      y: 0,
+      delay: 0.5,
+      scrollTrigger: {
+        trigger: p,
+        start: "top 80%",
+        end: "bottom 50%",
+        toggleActions: "play none none none",
+        scrub: 1,
+      },
+    });
+
+    chars.forEach((char, i) => {
+      gsap.fromTo(
+        char,
+        {
+          autoAlpha: 0,
+          y: "+=20",
+          color: "#222222",
+        },
+        {
+          autoAlpha: 1,
+          y: 0,
+          duration: 0.5,
+          color: "#fbfbfb",
+          delay: i * 0.05,
+          scrollTrigger: {
+            trigger: p,
+            start: "top 80%",
+            end: "bottom 50%",
+            toggleActions: "play none none none",
+            scrub: 1,
+          },
+        }
+      );
+    });
+  });
+
+  // Add the image animation
+  // select all images
+  const images = document.querySelectorAll(".aboutSection img");
+
+  // loop over each image
+  images.forEach((image) => {
+    // create a timeline for each image
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: image,
+        start: "top 70%",
+        end: "bottom 40%",
+        toggleActions: "play none none none",
+        scrub: 1,
+      },
+    });
+
+    // add animation to the timeline for each image
+    tl.from(image, {
+      duration: 1,
+      opacity: 0,
+      ease: "power2.inOut",
+    });
+  });
+
+  //h2 effect
+  const headers = document.querySelectorAll(".about h2");
+  const master = gsap.timeline({ paused: true });
+
+  // loop over each header element
+  headers.forEach((header) => {
+    // create a SplitText instance for the current header element
+    const split = new SplitText(header, { type: "chars" });
+
+    // create a timeline for the current header element
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: header,
+        start: "top 70%",
+        end: "bottom 40%",
+        toggleActions: "play none none none",
+        scrub: 1,
+      },
+    });
+
+    // add animations to the timeline for the current header element
+    tl.from(split.chars, {
+      duration: 1,
+      y: 100,
+      opacity: 0,
+      rotationX: 180,
+      transformOrigin: "0% 20% -20",
+      ease: "back.out(1.7)",
+      stagger: 0.05,
+    }).to(header, {
+      duration: 0.5,
+      opacity: 1,
+    });
+
+    // add the timeline to the master timeline
+    master.add(tl);
+  });
+
+  // play the master timeline when the page loads
+  master.play();
+});
 </script>
 
 <style>
+.aboutSection span {
+  @apply m-auto  block max-w-[12em] mt-12;
+}
+.aboutSection p {
+  @apply leading-loose text-[1.3rem] my-4;
+}
+.aboutSection img {
+  @apply w-[60%] m-auto;
+}
 
+.blur {
+  @apply fixed -bottom-2 w-screen left-0 h-[40%];
+  filter: blur(5px);
+  background: linear-gradient(
+    to bottom,
+    rgba(34, 34, 34, 0.7),
+    rgba(34, 34, 34, 1)
+  );
+}
 </style>
