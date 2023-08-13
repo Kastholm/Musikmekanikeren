1c8032717c1c15d820e5c60a68a0fe4a088336ef
Anton B. Christensen <webbermanden@gmail.com>
2023-08-13 15:43:21 +0200

    Shortcuts opdateret efter to-do liste

--- components/Base/Shortcuts.vue
+++ components/Base/Shortcuts.vue
@@ -4,16 +4,7 @@
     class="px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto relative bg-smooth "
   >
     <!-- Title -->
-    <div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
-      <h2
-        class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-cyangreen"
-      >
-        Lær mere om Musikmekanikeren
-      </h2>
-      <p class="mt-1 text-cyangreen dark:text-metric">
-        Stay in the know with insights from industry experts.
-      </p>
-    </div>
+    
     <!-- End Title -->
 
     <!-- Grid -->
@@ -44,9 +35,9 @@
             instrumenter som har været igennem værkstedet..
           </p>
           <p
-            class="mt-5 inline-flex items-center gap-x-1.5 text-blue-600 decoration-2 group-hover:underline font-medium"
+            class="mt-5 inline-flex items-center gap-x-1.5 text-[#304a4d] decoration-2 group-hover:underline font-medium"
           >
-            Read more
+            Læs mere
             <svg
               class="w-2.5 h-2.5"
               width="16"
@@ -80,16 +71,15 @@
           <h3
             class="text-xl font-semibold text-cyangreen group-hover:text-cyangreen dark:text-cyangreen"
           >
-            Hvem er jeg ?
+            Hvem står bag Musik Mekanikeren?
           </h3>
           <p class="mt-3 text-cyangreen dark:text-cyangreen whitespace-normal">
-            Optimize your in-person experience with best-in-class capabilities
-            like badge printing and lead retrieval
+            Læs mere om mig og min erfaring med musikinstrumenter og forstærkere igennem <br>de sidste 16 år..
           </p>
           <p
-            class="mt-5 inline-flex items-center gap-x-1.5 text-blue-600 decoration-2 group-hover:underline font-medium"
+            class="mt-5 inline-flex items-center gap-x-1.5 text-[#304a4d] decoration-2 group-hover:underline font-medium"
           >
-            Read more
+            Læs mere
             <svg
               class="w-2.5 h-2.5"
               width="16"
@@ -111,14 +101,14 @@
 
       <!-- Card -->
       <a
-        class="group relative flex flex-col w-full min-h-[15rem] bg-center bg-cover rounded-xl hover:shadow-lg transition bg-[url('https://images.unsplash.com/photo-1634017839464-5c339ebe3cb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3000&q=80')]"
+        class="group relative flex flex-col w-full min-h-[15rem] bg-center bg-cover rounded-xl hover:shadow-lg transition bg-[url('https://i.ibb.co/JHHXnF2/chris-hardy-xu-47ojzho8-unsplash.jpg')]"
         href="#"
       >
         <div class="flex-auto p-4 md:p-6">
-          <h3 class="text-xl text-white/[.9] group-hover:text-white whitespace-normal text-left">
-            <span class="font-bold text-2xl">Besgøg vores Shop</span> 
-            <p>
-              Leder du efter nyt udstyr til dit instrument?
+          <h3 class="text-xl text-white/[.9] group-hover:text-white whitespace-normal ">
+            <span class="font-bold text-3xl">Kontakt os her på siden</span> 
+            <p class=" text-xltranslate-y-4">
+              Vi hjælper dig med alt der omhandler instrumenter og forstærkere
             </p>
           </h3>
         </div>
@@ -126,7 +116,7 @@
           <div
             class="inline-flex items-center gap-2 text-sm font-medium text-white group-hover:text-white/[.7]"
           >
-            Visit the site
+            Kontakt
             <svg
               class="w-2.5 h-2.5"
               width="16"
